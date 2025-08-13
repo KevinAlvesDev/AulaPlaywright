e919aaa69af3eebb0442c1b74efb5806906b39bf
Kevin Alves Dev <Kevinalveswp@gmail.com>
2025-08-13 09:44:43 -0300

    waitForLoadState

--- tests/Yahoo.spec.ts
+++ tests/Yahoo.spec.ts
@@ -3,8 +3,8 @@ test('Buscar "Pacto Soluções" no Yahoo e verificar link', async ({ page }) => 
   await page.goto('https://br.yahoo.com/');
   await page.fill('input[name="p"]', 'Pacto Soluções');
   await Promise.all([
-    page.waitForNavigation({ waitUntil: 'domcontentloaded' }),
-    page.keyboard.press('Enter'),
+  await page.keyboard.press('Enter'),
+  await page.waitForLoadState('domcontentloaded'),
   ]);
   await expect(page.locator('a[href="https://sistemapacto.com.br/"]')).toBeVisible();
 });
